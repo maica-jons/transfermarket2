5872117903110cfdf5e4c983c79ee363da1836df
juanmaibarra <jibarra@itba.edu.ar>
2023-06-01 17:19:14 -0300

    Update Persona.py

---
+++ Persona.py
@@ -0,0 +1,16 @@
+import datetime
+import math
+
+class Persona():
+
+    lista_personas = []
+    lista_dni_personas = []
+
+    def __init__(self, nombre, apellido, dni, edad, nacionalidad, estatura, peso):  
+        self.nombre = nombre
+        self.apellido = apellido
+        self.dni = dni
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+        self.estatura = estatura
+        self.peso = peso
